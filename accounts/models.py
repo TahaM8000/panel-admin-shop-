5e0695435c21f8db11b77dd203b8bc72bac37eae
Taha <TahaM8000@gmail.com>
2022-07-30 12:31:00 +0430

    start project

---
+++ accounts/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, AbstractUser
+from .managers import *
+
+# ----------------------------------------------------------------------------------------------------------------------------
+class User(AbstractBaseUser):
+    phoneNumber = models.CharField(unique=True, max_length=11)
+    firstName = models.CharField(max_length=100, null=True, blank=True)
+    lastName = models.CharField(max_length=100, null=True, blank=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    code = models.IntegerField(blank=True,null=True)
+
+    # receipt = models.ManyToManyField(Receipt ,blank=True,related_name="user")
+
+    USERNAME_FIELD = 'phoneNumber'
+    objects = MyUserManager()
+
+    def __str__(self):
+        return str(self.phoneNumber) + " - " + str(self.firstName) + " " + str(self.lastName)
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+# ----------------------------------------------------------------------------------------------------------------------------
